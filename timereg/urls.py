fb9b5d31d00443244ff905226fbcc5244ba44f1d
Fredrik Hallberg <saccer92@gmail.com>
2015-05-03 10:52:26 +0200

    Added views and templates. We can now list shifts on a per month basis

---
+++ timereg/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^entershifts/$', views.entershifts, name='entershifts'),
+    url(r'^(?P<user>[0-9]+)/showreport/(?P<year>[0-9]+)/(?P<month>[0-9]+)$', views.showreport, name='showreport'),
+]
--- timereg/views.py
+++ timereg/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from timereg.models import Shift, ShiftFragment
+from datetime import time, datetime
+from django.template import RequestContext, loader
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index page.")
+
+
+
+def entershifts(request):
+    return HttpResponse("Hello, here you can enter your shifts")
+
+def showreport(request, user, year, month):
+    shift_list = Shift.objects.all().filter(worker__exact=user,start_time__year=year,start_time__month=month) # Only checks start_date.
+    
+    template = loader.get_template('timereg/showreport.html')
+    context = RequestContext(request, {'shift_list' : shift_list})
+    return HttpResponse(template.render(context))
+
