aa494540eaf806e5e93ee0e1aa8079ecd2101f5f
Fredrik Hallberg <saccer92@gmail.com>
2015-08-14 06:48:49 +0200

    Added main index page

--- timez/urls.py
+++ timez/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('timereg.urls', namespace="mainindex")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^timereg/', include('timereg.urls', namespace="timereg")),
     url(r'^accounts/', include('registration.backends.simple.urls')),
